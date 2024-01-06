9f9b797b92cc81bc173e6dd098eb923eb93d4622
Bora Kaya <kayabo19@itu.edu.tr>
2024-01-06 16:35:46 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -27,8 +27,8 @@ del dfas["Unnamed: 0"]
 dfas=dfas.rename_axis(["Tarih"])
 
 fig1 = go.Figure()
-fig1.add_trace(go.Scatter(x=yıllıktahmin.index[:25],y=yıllıktahmin["Ortalama"].iloc[:25],mode='lines',name="Enflasyon"))
-fig1.add_trace(go.Scatter(x=yıllıktahmin.index[24:],y=yıllıktahmin["Ortalama"].iloc[24:-2],mode='lines',name="Tahmin"))
+fig1.add_trace(go.Scatter(x=yıllıktahmin.index[:24],y=yıllıktahmin["Ortalama"].iloc[:24],mode='lines',name="Enflasyon"))
+fig1.add_trace(go.Scatter(x=yıllıktahmin.index[23:],y=yıllıktahmin["Ortalama"].iloc[23:-2],mode='lines',name="Tahmin"))
 fig1.add_trace(go.Scatter(x=yıllıktahmin.index[20:24],y=[61.94,60.84,62.18,64.70],mode='markers',name="Geçmiş Tahminler"))
 fig1.update_traces(line=dict(width=3)) 
 fig1.update_layout(font_family="Arial Black",
